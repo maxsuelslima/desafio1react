f60ba90d8355bc026c978e1891cc22cac7da3df2
Maxsuel Lima <maxsuelslima@gmail.com>
2021-09-14 23:13:06 -0300

    testandoooo

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react'
 
 import '../styles/tasklist.scss'
 //testando commit
+//asasas
 import { FiTrash, FiCheckSquare } from 'react-icons/fi'
 import { numberLiteralTypeAnnotation } from '@babel/types';
 
